78d62d58240b1d4dadd52305f92a24d1b641a1f3
Log-O <elektrikminsk@gmail.com>
2024-10-02 23:00:50 +0200

    послед

--- script_if.js
+++ script_if.js
@@ -1,21 +1,37 @@
-let surmame = prompt('Введите вашy фамилию');
-let ima = prompt('Введите ваше имя');
-let father = prompt('Введите вашe отчество');
-let age = parseInt(prompt(' Введите ваш возраст в годах'));
-let sex = confirm('ваш пол - мужской?');
-let pol; 
-let plus5 = age + 5;
+let surname;
+do {
+    surname= prompt('Введите вашy фамилию');
+} while (!surname);
 
-if ( sex === true )  pol = "мужской";  else  pol = "женский";
-let pensia;
-if ( sex === true &&  age >= 65  )  {pensia = "да";} 
-else  if ( sex === false &&  age >= 60 ) {pensia = "да";}
-else {pensia = "нет";}
-let days = ( age*365 );
+let ima;
+do {
+    ima = prompt('Введите ваше имя');
+} while (!ima);
 
-alert("ваше ФИО:" + " " + surmame + " " + ima + " " + father + "\n" 
-    + "ваш возраст в годах:" + " " + age + "\n" 
-    + "ваш возраст в днях:" + " " +  days + "\n" 
-    + "через 5 лет вам будет:" + " " + plus5 + "\n"
-    + "ваш пол:" + " " +  pol + "\n"
-    + "вы на пенсии:" + " " +  pensia );
+let father;
+do {
+    father = prompt('Введите вашe отчество');
+} while (!father);
+
+let ageS, age;
+do {
+    ageS = prompt('Введите ваш возраст в годах');
+    age = parseInt(ageS)
+} while (isNaN(age));
+
+
+const gender=confirm("Введите пол");
+
+const pensStr= ((gender && age>=65)||(!gender && age>=60))?"да":"нет";
+
+
+alert(
+`
+ваше ФИО:  ${surname} ${ima} ${father}
+ваш возраст в годах: ${age}
+ваш возраст в днях: ${age*365}
+через 5 лет вам будет: ${age+5}
+ваш пол: мужской  ${gender?"M":"Ж"}
+вы на пенсии: ${pensStr}
+`
+);
